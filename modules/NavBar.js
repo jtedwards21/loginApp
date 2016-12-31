fbbbb53f25689cfceeeb13ce5c2ab5e850c355a5
James Ahlm <14782828428@qq.com>
2016-12-31 11:47:59 +0800

    Added Links to the top

--- modules/NavBar.js
+++ modules/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   getInitialState(){
@@ -15,13 +16,13 @@ export default React.createClass({
         <span className="icon-bar"></span>
         <span className="icon-bar"></span>
       </button>
-      <a className="navbar-brand" href="#">Brand</a>
+      <Link className="navbar-brand" to="/"><span id="glass" className="glyphicon glyphicon-glass"></span></Link>
     </div>
 
     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul className="nav navbar-nav">
         <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-        <li><a href="#">Link</a></li>
+        <li><Link to="/dashboard" href="#">Dashboard</Link></li>
         <li className="dropdown">
           <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
           <ul className="dropdown-menu">
@@ -35,20 +36,14 @@ export default React.createClass({
           </ul>
         </li>
       </ul>
-      <form className="navbar-form navbar-left">
-        <div className="form-group">
-          <input type="text" className="form-control" placeholder="Search"/>
-        </div>
-        <button type="submit" className="btn btn-default">Submit</button>
-      </form>
       <ul className="nav navbar-nav navbar-right">
         <li><a href="#">Link</a></li>
         <li className="dropdown">
-          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span className="glyphicon glyphicon-menu-hamburger"></span> <span className="caret"></span></a>
           <ul className="dropdown-menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
+            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/signin">Sign In</Link></li>
+            <li><Link to="/logout">Log Out</Link></li>
             <li role="separator" className="divider"></li>
             <li><a href="#">Separated link</a></li>
           </ul>
