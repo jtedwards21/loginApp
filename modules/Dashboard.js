b0459036d1e8c416a369afc7b860eed1bc5e5b25
James Ahlm <14782828428@qq.com>
2016-12-31 19:43:07 +0800

    Change Dashboard style

--- modules/Dashboard.js
+++ modules/Dashboard.js
@@ -6,14 +6,15 @@ export default React.createClass({
   render() {
     return (
       <div　id="dashboard" className="col-sm-12">
-        <div className="section-title text-center">Dashboard</div>
-	<div className="" id="dashboard-pills">
+	<div className="col-sm-3" id="dashboard-pills">
 	  <ul className="nav nav-pills nav-stacked">
             <li role="presentation" className="active"><Link to="/userinfo">User Info</Link></li>
 	    <li role="presentation"><Link to="/goin">Goin</Link></li>
 	  </ul>
 	</div>
+	<div className="col-sm-9" id="current-pill">
 	{this.props.children}
+	</div>
       </div>
     )
   }
