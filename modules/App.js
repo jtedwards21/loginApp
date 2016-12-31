e3cfbb7b10eb10fa7ddd74a9d67f48d53d398b0f
James Ahlm <14782828428@qq.com>
2016-12-31 11:37:52 +0800

    Added a navbar

--- modules/App.js
+++ modules/App.js
@@ -3,6 +3,7 @@ import NavLink from './NavLink'
 import auth from './../public/auth'
 import { Link } from 'react-router'
 import axios from 'axios'
+import NavBar from './NavBar'
 
 export default React.createClass({
 　　getInitialState(){
@@ -17,16 +18,17 @@ export default React.createClass({
   
   render() {
     return (
-      <div>
-        <h1>React Router Login</h1>
-	<ul>
-	　　<Link to="/signup">Sign up</Link>
-	  {this.state.loggedIn ? (
-	  <Link to="/logout">Log out</Link>
-	) : (
-	  <Link to="/login">Sign in</Link>
-	)}	
-	</ul>
+      <div className="row">
+        <NavBar />
+        <h1 id="title" className="text-center col-sm-12">What's Goin' On Tonight?</h1>
+	  <ul id="nav">
+	　　  <Link to="/signup">Sign up</Link>
+	    {this.state.loggedIn ? (
+	    <Link to="/logout">Log out</Link>
+	  ) : (
+	    <Link to="/login">Sign in</Link>
+	  )}	
+	  </ul>
 	  {this.state.loggedIn ? (<p>You are logged in</p>):(<p>You are not logged in</p>)}
         <ul role="nav">
           <li><NavLink to="/" onlyActiveOnIndex>Home</NavLink></li>
@@ -34,7 +36,6 @@ export default React.createClass({
           <li><NavLink to="/about">About</NavLink></li>
           <li><NavLink to="/repos">Repos</NavLink></li>
         </ul>
-	{this.state.response}
         {this.props.children}
       </div>
     )
